0146a42744f83ccd59da0ed2f16231743ce0f187
TheArqsz <38382850+TheArqsz@users.noreply.github.com>
2026-03-31 16:26:02 +0200

    refactor: Removed 'Community Edition workaround' text from settings

--- src/main/java/com/arqsz/burpgitleaks/ui/SettingsTab.java
+++ src/main/java/com/arqsz/burpgitleaks/ui/SettingsTab.java
@@ -418,7 +418,7 @@ public class SettingsTab extends JPanel implements SettingsPanel {
         c.gridy++;
         c.gridwidth = 2;
         JCheckBox issuesTabCb = new JCheckBox(
-                "[Experimental] Enable custom 'Issues' tab (Community Edition workaround)");
+                "[Experimental] Enable custom 'Issues' tab");
         issuesTabCb.setToolTipText("Displays a custom table of findings as a separate top-level tab.");
         issuesTabCb.setSelected(settings.isShowIssuesTab());
         issuesTabCb.addActionListener(e -> {
